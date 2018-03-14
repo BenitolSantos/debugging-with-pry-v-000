b6b8eadb8556fc23ba15d1048abc645caa6e1806
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-14 19:07:06 +0000

    automatically backed up by learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -1,5 +1,5 @@
 require 'pry'
-
+#learn and ruby both turn on pry
 def prying_into_the_method
 	inside_the_method = "We're inside the method"
 	puts inside_the_method
@@ -10,3 +10,4 @@ def prying_into_the_method
 end
 
 prying_into_the_method
+
